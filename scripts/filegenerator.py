2527f827fa8a19a3c19a13521ee2d67a1415b2de
f1rset <stekostant@gmail.com>
2025-02-13 14:20:00 +0200

    prog1 runner

--- scripts/filegenerator.py
+++ scripts/filegenerator.py
@@ -1,14 +1,15 @@
 """
 
 """
-from random import randint
+def generate():
+    from random import randint
 
-with open("data/test.txt", 'w', encoding="utf-8") as file:
-    sum = 0
-    for i in range(10000):
-        a = randint(0, 10**8)
-        sum += a
-        file.write(str(a))
-        file.write("\n")
+    with open("data/test.txt", 'w', encoding="utf-8") as file:
+        sum = 0
+        for i in range(5000):
+            a = randint(0, 10**8)
+            sum += a
+            file.write(str(a))
+            file.write("\n")
 
-print(sum)
+    print(sum)
